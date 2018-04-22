00b47b99d0b10374d9d6536e8af592de85677bf7
Jason Yoo <jasonyoo0116@gmail.com>
2018-04-22 16:04:52 -0700

    analysis page complete

--- src/assets/bootstrap.js
+++ src/assets/bootstrap.js
@@ -1,7 +1,7 @@
-import { Button, ButtonGroup, ButtonToolbar, Col, Grid, Jumbotron, ListGroup, ListGroupItem, Row } from 'react-bootstrap/lib';
+import { Button, ButtonGroup, ButtonToolbar, Col, Grid, Jumbotron, ListGroup, ListGroupItem, Row, Table, } from 'react-bootstrap/lib';
 import bootstrapClasses from 'bootstrap/dist/css/bootstrap.min.css';
 
 export default {
 	bootstrapClasses,
 };
-export { Button, ButtonGroup, ButtonToolbar, Col, Grid, Jumbotron, ListGroup, ListGroupItem, Row,};
+export { Button, ButtonGroup, ButtonToolbar, Col, Grid, Jumbotron, ListGroup, ListGroupItem, Row, Table, };
--- src/constants/questions.js
+++ src/constants/questions.js
@@ -1,3 +1,4 @@
+// score can be 3 2 or 1
 const DIAGNOSTICS_QUESTIONS = {
 	"1": {
 		question: "Do you do your best to hold a strong intention of sustaining the attention on the breathe during meditation?",
@@ -8,11 +9,11 @@ const DIAGNOSTICS_QUESTIONS = {
 		],
 		suggestions: [
 			{
-				score: 5, 
+				score: 3, 
 				message: "That's perfect - Continue to be mindful and harbour the right intentions."
 			},
 			{
-				score: 3,
+				score: 2,
 				message: "Continue to remind yourself to hold a conscious intention of sustaining the attention on the breathe."
 			},
 			{
@@ -35,11 +36,11 @@ const DIAGNOSTICS_QUESTIONS = {
 				message: "Find and focus on the joyful sensation. Remember that only bad meditation session is one not done.",
 			},
 			{
-				score: 5,
+				score: 3,
 				message: "That's perfect - Continue to develop your attention and awareness for increased clarity.",
 			},
 			{
-				score: 1,
+				score: 2,
 				message: "Try to expand your peripheral awareness to notice the dullness and gently tighten up your focus on the breathe.",
 			},
 		],
@@ -54,11 +55,11 @@ const DIAGNOSTICS_QUESTIONS = {
 		],
 		suggestions: [
 			{
-				score: 5, 
+				score: 3, 
 				message: "That's perfect - Continue to be keep up the effort to apply your mindfulness."
 			},
 			{
-				score: 3,
+				score: 2,
 				message: "Increase your efforts to be mindful in everyday setting."
 			},
 			{
