ab629c4cb61b2bcc1e9aa13447ad38be594040a2
Jason Yoo <jasonyoo0116@gmail.com>
2018-04-22 23:20:52 -0700

    mock graph drawn

--- src/components/diagnostics/DiagnosticsResults.js
+++ src/components/diagnostics/DiagnosticsResults.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
 import bootstrapClasses, { Col, Grid, Jumbotron, Row,} from '../../assets/bootstrap';
 import { ContainerDiv, SimpleDiv } from '../common/ContainerDiv';
 import { db } from '../../firebase';
 import * as routes from '../../constants/routes';
 import classes from './Diagnostics.css';
+
+Charts(FusionCharts);
 
 class DiagnosticsResultsPage extends React.Component {
 	constructor(props, { authUser }) {
@@ -15,15 +20,60 @@ class DiagnosticsResultsPage extends React.Component {
 		};
 
 	}
+	renderFakeChart = () => {
+		const myDataSource = {
+		  chart: {
+		    caption: 'Diagnostics Results',
+		    subCaption: 'Areas for Improvement From 5 Most Recent Entries',
+		  },
+		  data: [
+		    {
+		      label: 'Q1',
+		      value: '3',
+		    },
+		    {
+		      label: 'Q2',
+		      value: '3',
+		    },
+		    {
+		      label: 'Q3',
+		      value: '2',
+		    },
+		    {
+		      label: 'Q4',
+		      value: '2',
+		    },
+		    {
+		      label: 'Q5',
+		      value: '1',
+		    },
+		  ],
+		};
+		const chartConfigs = {
+		  type: 'column2d',
+		  width: '100%',
+		  height: 300,
+		  dataFormat: 'json',
+		  dataSource: myDataSource,
+		};
+		return <ReactFC {...chartConfigs}/>
+	}
 	componentDidMount() {
 		db.getResponses(this.state.authUser.uid).then(snapshot => {
 			const entriesOnly = snapshot.val();
 			this.setState({ allResponses: entriesOnly });
-		})
+		});
 	}
 	render() {
+		if (!!this.state.allResponses) {
+			return (
+				<ContainerDiv>
+					{this.renderFakeChart()}
+				</ContainerDiv>
+			)
+		}
 		return (
-			<ContainerDiv>I am result {!!this.state.allResponses && this.state.allResponses.toString() }</ContainerDiv>
+			<ContainerDiv></ContainerDiv>
 		)
 	}
 }
