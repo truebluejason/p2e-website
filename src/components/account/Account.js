e7195ef55f4b66b505a53d39a59c62b941da672b
Jason Yoo <jasonyoo0116@gmail.com>
2018-05-03 09:09:56 -0700

    changed account page a little

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { ContainerDiv, SimpleDiv } from '../common/ContainerDiv';
 import { PasswordForgetForm } from '../login/PasswordForget';
 import PasswordChangeForm from '../login/PasswordChange';
 import withAuthorization from '../higherorder/withAuthorization';
 
-const AccountPage = (props, { authUser, userInfo }) =>
-  <div>
-    <h1>Account: {authUser.email}</h1>
-    <h1>Stage: {userInfo.highestLevel.toString()}</h1>
-    <h1></h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+class AccountPage extends React.Component {
+	constructor(props, { authUser, userInfo }) {
+		super(props);
+		this.state = {
+			authUser: authUser,
+			userInfo: userInfo,
+		};
+	}
+	render() {
+		return (
+			<ContainerDiv>
+			    <h1>Account: {this.state.authUser.email}</h1>
+			    <h1>Stage: {this.state.userInfo.highestLevel.toString()}</h1>
+			    <h1></h1>
+			    <PasswordForgetForm />
+			    <PasswordChangeForm />
+		  	</ContainerDiv>
+		);
+	}
+}
 
 AccountPage.contextTypes = {
   authUser: PropTypes.object,
